bdf153b2fc0b7a550d48317588a5e028b10d7d43
piyushgupta1319 <guptapiyush6336@gmail.com>
2026-06-17 17:41:08 +0530

    Remove Facebook provider temporarily

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,18 @@
+import Credentials from "next-auth/providers/credentials";
+
 export default {
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize() {
+        return null;
+      },
+    }),
+  ],
+
   pages: {
     signIn: "/auth/login",
   },
