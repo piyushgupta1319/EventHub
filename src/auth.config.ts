51a2803913331ef90fa92929385ce6a680c8201c
piyushgupta1319 <guptapiyush6336@gmail.com>
2026-06-17 17:36:08 +0530

    Fix auth config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,18 +1,4 @@
-import Credentials from "next-auth/providers/credentials";
-
 export default {
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize() {
-        return null;
-      },
-    }),
-  ],
-
   pages: {
     signIn: "/auth/login",
   },
