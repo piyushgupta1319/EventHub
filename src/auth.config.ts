f01ccab27f41003d229b095d68601e26bb988e8a
piyushgupta1319 <guptapiyush6336@gmail.com>
2026-06-21 01:27:27 +0530

    Fix auth config for middleware

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,15 @@
+import Credentials from "next-auth/providers/credentials";
+
 export default {
+  providers: [
+    Credentials({
+      credentials: {},
+      async authorize() {
+        return null;
+      },
+    }),
+  ],
+
   pages: {
     signIn: "/auth/login",
   },
